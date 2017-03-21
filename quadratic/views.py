cbc9d7f3c2ae214d00ee572cdffbaed90859c11d
Dmitri Goldshtein <israodin@gmail.com>
2017-03-21 23:53:09 +0200

    quadratic

---
+++ quadratic/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from quadratic_handler import get_parametr,QuadraticEducation
+# Create your views here.
+
+def results(request,a,b,c):
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    render()
+
