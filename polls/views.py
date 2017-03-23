2a91129e634c2cded650ca947c466b9a715d8c08
mazart <am.magicsmile@gmail.com>
2017-03-23 22:55:15 +0200

    KZ4 final changes

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-#from django.http import Http404
-#from django.template import loader
 
 from .models import Choice, Question
 
