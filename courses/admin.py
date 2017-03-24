8d3d164a725b1e6b831e67e41319931c11c587af
dipperside <figarich@gmail.com>
2017-03-24 20:06:55 +0200

    KZ 5-1: Models filled with data

--- courses/admin.py
+++ courses/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Course, Lesson
 
-admin.site.register(Course, Lesson)
+admin.site.register(Course) #, Lesson)
+admin.site.register(Lesson)
