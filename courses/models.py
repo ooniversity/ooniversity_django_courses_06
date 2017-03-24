de93d51b11dfe33f55a0ece7f6c405cf774424c0
mazart <am.magicsmile@gmail.com>
2017-03-24 06:01:56 +0200

    Task 5_2 first changes

---
+++ courses/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Course(models.Model):
+	name = models.CharField(max_length=254) # название курса
+	short_description = models.CharField(max_length=254) # краткое описание
+	description = models.TextField() # полное описание
+
+	def __str__(self): # функция возвращает название курса
+		return self.name 
+
+class Lesson(models.Model):
+	subject = models.CharField(max_length=254) # тема
+	description = models.TextField() # описание
+	course = models.ForeignKey(Course) # курс
+	order = models.PositiveIntegerField() # номер по порядку
+
+	def __str__(self): # функция возвращает тему урока
+		return self.subject
