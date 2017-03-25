411356ca701f439030e2e6829b26f01d1dbdf6b7
mazart <am.magicsmile@gmail.com>
2017-03-25 21:42:14 +0200

    KZ 5_2

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ['name', 'short_description']
+	search_fields = ['name']
+
+#class LessonInline
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['name', 'email', 'skype']
+	search_fields = ['surname', 'email']
+	list_filter = ['courses']
+
+admin.site.register(Student, StudentAdmin)
