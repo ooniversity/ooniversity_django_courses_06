72c8a5eb2fa39c78f4c630ac7e5bcfe984c05666
Eduard Striukov <edstrkv@gmail.com>
2017-03-25 09:32:10 +0200

    week3/kz5.2.03

--- students/admin.py
+++ students/admin.py
@@ -6,7 +6,8 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'email', 'skype']
     fieldsets = [
             ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
-            ('Contact info',  {'fields': ['email', 'phone', 'address', 'skype', 'courses']})
+            ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+            (None, {'fields': ['courses']})
             ]
     list_filter = ['courses']
     search_fields = ['surname', 'email']
