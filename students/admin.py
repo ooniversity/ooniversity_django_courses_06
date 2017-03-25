7b743be4bea577a9d4708646d8a048edc5a6ff5b
Eduard Striukov <edstrkv@gmail.com>
2017-03-25 09:54:33 +0200

    week3/kz5.2.05

--- students/admin.py
+++ students/admin.py
@@ -4,12 +4,13 @@ from students.models import Student
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'email', 'skype']
-    fieldsets = [
-            ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
-            ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
-            (None, {'fields': ['courses']})
-            ]
     list_filter = ['courses']
     search_fields = ['surname', 'email']
     filter_horizontal = ['courses']
+    fieldsets = [
+        ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+        ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+        (None, {'fields': ['courses']})
+        ]
+
 admin.site.register(Student, StudentAdmin)
