af14c2038ffc2f5b2faa23a813718751cce1a856
Burlakov Alexey <alex.tsumibito@gmail.com>
2017-03-25 21:39:43 +0200

    kz 5.1 v4

--- students/urls.py
+++ students/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = 'students'
 urlpatterns = [
-	url(r'^$', views.list_view, name='list_view'),
+    url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<student_id>[0-9]+)/$', views.detail, name='detail'),
 ]
