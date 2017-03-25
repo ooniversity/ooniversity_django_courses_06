459058baffa46258ceccdaf97954143e0fe83c19
mazart <am.magicsmile@gmail.com>
2017-03-25 22:02:53 +0200

    KZ 5_2 v.1

--- courses/admin.py
+++ courses/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
+class LessonInline(admin.StackedInline):
+	model = Lesson
+	fields = ['subject', 'description', 'order']
+
+class LessonAdmin(admin.ModelAdmin):
+	pass
+
 class CourseAdmin(admin.ModelAdmin):
 	list_display = ['name', 'short_description']
 	search_fields = ['name']
+	inlines = [LessonInline]
 
 #class LessonInline
 
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
