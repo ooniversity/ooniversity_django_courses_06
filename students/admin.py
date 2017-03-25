9d36175d3125a0ff3f8ac16ed96777f246938089
mazart <am.magicsmile@gmail.com>
2017-03-25 23:06:42 +0200

    KZ 5_2 v.3

--- students/admin.py
+++ students/admin.py
@@ -3,6 +3,14 @@ from students.models import Student
 
 class StudentAdmin(admin.ModelAdmin):
 	list_display = ['name', 'email', 'skype']
+	fieldsets = (
+		('Personal info', {
+			'fields': ('name', 'surname', 'date_of_birth')
+		}), 
+		('Contact info', {
+			'fields': ('email', 'phone', 'address', 'skype', 'courses')
+		}),
+	)
 	search_fields = ['surname', 'email']
 	list_filter = ['courses']
 
