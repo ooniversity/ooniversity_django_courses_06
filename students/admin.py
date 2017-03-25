0cb82e6ca6ec9ecd38068db06d6d7f545f2d06a2
mazart <am.magicsmile@gmail.com>
2017-03-25 23:35:28 +0200

    KZ 5_2 v.4

--- students/admin.py
+++ students/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from students.models import Student
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ['name', 'email', 'skype']
+	list_display = ['full_name', 'email', 'skype']
 	fieldsets = (
 		('Personal info', {
 			'fields': ('name', 'surname', 'date_of_birth')
