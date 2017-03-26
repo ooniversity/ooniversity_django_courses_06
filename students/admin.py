970cc66131555f213cb01e38c64b179c96ae9c74
dipperside <figarich@gmail.com>
2017-03-26 13:14:07 +0300

    KZ 5-2: i dont want to be admingit status !

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from students.models import Student
+from django.db import models
+from django.forms import widgets, ModelMultipleChoiceField
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
-# Register your models here.
-from .models import Student
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_display = ['full_name', 'email', 'skype']
+    list_filter = ['courses']
+    filter_horizontal = ['courses']
+    
+    fieldsets = [('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+                 ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+                 (None, {'fields': ['courses']}),]
+                
+
+    def full_name(self, obj):
+        return str(obj.name + ' ' + obj.surname)
+
+
+admin.site.site_header = 'PyBursa Administration'
+admin.site.register(Student, StudentAdmin)
