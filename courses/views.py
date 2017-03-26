2dd19a453a98e55e8071c4cfae7b5e1068ec3c22
Alexandr Fedorchenko <smolnax@gmail.com>
2017-03-26 22:44:17 +0300

    Kz6

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Course, Lesson
+from coaches.models import Coach
 
 # Create your views here.
 
@@ -7,5 +8,7 @@ def detail(request, course_id):
     
     course = Course.objects.get(id=course_id)
     lessons_list = Lesson.objects.filter(course=course)
+    coach = Coach.objects.get(id=course.coach.id)
+    assistant = Coach.objects.get(id=course.assistant.id)
 
-    return render(request, "courses/detail.html", {'course':course, 'lessons_list': lessons_list})
+    return render(request, "courses/detail.html", {'course':course, 'lessons_list': lessons_list, 'coach':coach, 'assistant':assistant})
