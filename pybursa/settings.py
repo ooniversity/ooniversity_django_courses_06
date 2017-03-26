d358e4841f4c48c2ac09f97609d56dd50476b5e4
Burlakov Alexey <alex.tsumibito@gmail.com>
2017-03-26 12:09:00 +0300

    kz 5.1 v6

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en_us'
 
 TIME_ZONE = 'Europe/Kiev'
 
