f9cd1e85251c8f07c174817ec9c8c6086a1541a9
LeusMaximus <leus.maximus@gmail.com>
2017-03-26 21:01:57 +0300

    refactoring

--- students/models.py
+++ students/models.py
@@ -5,7 +5,7 @@ from courses.models import Course
 class Student(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField()
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=300)
