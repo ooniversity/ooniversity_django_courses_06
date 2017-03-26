7819eb0bec96f49874f3552542229ce378e36d26
mazart <am.magicsmile@gmail.com>
2017-03-26 11:02:05 +0300

    KZ 6

---
+++ coaches/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Coach(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	date_of_birth = models.DateField()
+	gender = models.CharField(max_length=254, choices=(('M', 'Male'), ('F', 'Female')))
+	phone = models.CharField(max_length=254)
+	address = models.CharField(max_length=254)
+	skype = models.CharField(max_length=254)
+	description = models.TextField()
+
+	def __str__(self):
+		return self.user.username	
