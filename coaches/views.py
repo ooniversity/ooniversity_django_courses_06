51c5cfb0efc2a6b2fc028017baad2e5d05834e54
Burlakov Alexey <alex.tsumibito@gmail.com>
2017-03-28 12:10:46 +0300

    kz 6 v8

--- coaches/views.py
+++ coaches/views.py
@@ -4,12 +4,11 @@ from courses.models import Course
 
 
 def detail(request, coach_id):
-    coach = Coach.objects.filter(id=int(coach_id)).first()
-    course_couch = Course.objects.filter(coach = coach.id)
-    course_assist = Course.objects.filter(assistant = coach.id)
-    print(course_couch)
+    coach = Coach.objects.get(id=int(coach_id))
+    coach_courses = Course.objects.filter(coach = coach.id)
+    assistant_courses = Course.objects.filter(assistant = coach.id)
     return render(request, 'coaches/detail.html', {
 				    'coach': coach,
-				    'course_couch': course_couch,
-				    'course_assist': course_assist,	
+				    'coach_courses': coach_courses,
+				    'assistant_courses': assistant_courses,	
 				    })
