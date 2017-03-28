0cce4f6ca9cb02084142c00645b64e438a8f8fbb
Burlakov Alexey <alex.tsumibito@gmail.com>
2017-03-28 11:51:30 +0300

    kz 6 v7

--- students/models.py
+++ students/models.py
@@ -3,14 +3,14 @@ from courses.models import Course
 
 
 class Student(models.Model):
-    name = models.CharField(verbose_name=u'имя', max_length=64)
-    surname = models.CharField(verbose_name=u'фамилия', max_length=64)
-    date_of_birth = models.DateField(verbose_name=u'дата рождения', null=True, blank=True) 
-    email = models.EmailField(unique=True, null=True )
-    phone = models.CharField(verbose_name=u'телефон', max_length=15)
-    address = models.CharField(verbose_name=u'адрес',max_length=255)
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    date_of_birth = models.DateField(null=True, blank=True) 
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
     skype = models.CharField(max_length=64)
-    courses = models.ManyToManyField(Course, verbose_name=u'курсы, на которых учится студент')
+    courses = models.ManyToManyField(Course)
     
     def __str__(self):
             return '%s %s' % (self.surname, self.name)
