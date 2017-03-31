c8643b04fc98fa18e5d514b5542a7486eb904a7c
Eduard Striukov <edstrkv@gmail.com>
2017-04-01 02:51:59 +0300

    week4/hw7.2.5 - i think it's right

--- quadratic/views.py
+++ quadratic/views.py
@@ -2,37 +2,31 @@ from django.shortcuts import render
 from quadratic.forms import QuadraticForm
 
 
+
 def quadratic_results(request):
-
-    context = {}
-    info = ''
-    diskr_value = ''
-
-
-    if request.method == "GET":
+    if len(request.GET) > 0:
         form = QuadraticForm(request.GET)
+        context = {}
+        context["form"] = form
+        context["diskr"] = ''
         if form.is_valid():
-            
             a = form.cleaned_data['a']
             b = form.cleaned_data['b']
             c = form.cleaned_data['c']
-
-            context['diskr_value'] = b**2 - 4*a*c
-            context['diskriminant'] = 'Дискриминант: '
-            if context['diskr_value'] < 0:
-
-                context['info'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-            elif context['diskr_value'] == 0:
-
-                x1 = x2 = (b + context['diskr_value'] ** (1/2.0)) / 2*a
+            diskr = b * b - 4 * a * c
+            context['diskr'] = "Дискриминант: %s" %diskr
+            if diskr < 0:
+                context["info"] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+            elif diskr == 0:
+                x1 = -b / 2 * a
                 context['info'] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x1
-            elif context['diskr_value'] > 0:
-
-                x1 = (-b + context['diskr_value'] ** (1/2.0)) / 2*a
-                x2 = (-b - context['diskr_value'] ** (1/2.0)) / 2*a
-                context['info'] = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
-
+            else:
+                x1 = (-b + diskr ** (1 / 2)) / 2 * a
+                x2 = (-b - diskr ** (1 / 2)) / 2 * a
+                context['info'] = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2) 
     else:
         form = QuadraticForm()
-    context['form'] = form
+        context = {'form': form}
     return render(request, 'quadratic/results.html', context)
+
+
