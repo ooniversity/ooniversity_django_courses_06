9597e022e840712764dd16db96c7e03516787e4a
Eduard Striukov <edstrkv@gmail.com>
2017-04-01 00:26:59 +0300

    week4/hw7.2.1 with some bugs about 'errors'

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class QuadraticForm(forms.Form):
-    a = forms.CharField(max_length=10)
-    b = forms.CharField(max_length=10)
-    c = forms.CharField(max_length=10)
+    a = forms.CharField(max_length=10, required=True, label='коэффициент a')
+    b = forms.CharField(max_length=10, required=True, label='коэффициент b')
+    c = forms.CharField(max_length=10, required=True, label='коэффициент c')
+
+    def clean_a(self):
+        data = self.cleaned_data['a']
+        if data == 0:
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        return data
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from quadratic.forms import QuadraticForm
 
-def validation(value, first_value=False):
 
+def validation(value, first_value=False):
     value_error = ''
     not_int_error = 'коэффициент не целое число'
     none_error = 'коэффициент не определен'
@@ -23,37 +23,46 @@ def calc_root(order = 1):
         x = (-int(b) - int(d) ** (1 / 2)) / 2 * int(a)
     return x
 
-
-
 def quadratic_results(request):
-    
     form = QuadraticForm()
-    #print(request.POST)
-    a = request.GET['a']
-    b = request.GET['b']
-    c = request.GET['c']
-    error_a = validation(request.GET['a'], True)
-    error_b = validation(request.GET['b'])
-    error_c = validation(request.GET['c'])
-    flag = False
-    diskr = None
-    messages = ''
-    x1 = ''
-    x2 = ''
-    if error_a == '' and error_b == '' and error_c == '':
-        flag = True
-        diskr = int(b) ** 2 - 4 * int(a) * int(c)
-        if diskr < 0:
-            messages = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-        elif diskr == 0:
-            x1 = x2 = (-int(b) + int(diskr) ** (1 / 2)) / 2 * int(a)
-            messages = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x1
-        elif diskr > 0:
-            x1 = (-int(b) + int(diskr) ** (1 / 2)) / 2 * int(a)
-            x2 = (-int(b) - int(diskr) ** (1 / 2)) / 2 * int(a)
-            messages = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
+    print(request.POST)
+    print(request.GET)
+    context = {'form': form}
+    context['form'] = form
 
-    context = {'form': form, 'a': a, 'b': b, 'c': c, 'error_a': error_a, 'error_b': error_b, 'error_c': error_c, 'diskr': diskr, 'flag' : flag, 'messages': messages}
+    if request.method == "GET":
+        form = QuadraticForm(request.GET)
+        if form.is_valid():
+            print(form.cleaned_data['a'])
+            print(form.cleaned_data['b'])
+            print(form.cleaned_data['c'])
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            c = form.cleaned_data['c']
+            print(form.cleaned_data)
+            error_a = validation(a, True)
+            error_b = validation(b)
+            error_c = validation(c)
+            print(error_a)
+            flag = False
+            diskr = False
+            info = ''
+            x1 = ''
+            x2 = ''
+            if error_a == '' and error_b == '' and error_c == '':
+                flag = True
+                diskr = int(b) ** 2 - 4 * int(a) * int(c)
+                if diskr < 0:
+                    info = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+                elif descr == 0:
+                    x1 = x2 = (-int(b) + int(diskr) ** (1 / 2)) / 2 * int(a)
+                    info = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x1
+                elif descr > 0:
+                    x1 = (-int(b) + int(diskr) ** (1 / 2)) / 2 * int(a)
+                    x2 = (-int(b) - int(diskr) ** (1 / 2)) / 2 * int(a)
+                    info = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
+
+            context = {'a': a, 'b': b, 'c': c, 'error_a': error_a, 'error_b': error_b, 'error_c': error_c, 'diskr': diskr, 'flag' : flag, 'info': info, 'form': form}
+        else:
+            form = QuadraticForm()
     return render(request, 'quadratic/results.html', context)
-
-
