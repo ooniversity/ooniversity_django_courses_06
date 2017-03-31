7089d51fab2554f4aad41d13ac973f75ea68e3ae
Zoria1988 <zarina.bondarenko@gmail.com>
2017-03-31 23:12:07 +0300

    Add 7_1

--- courses/models.py
+++ courses/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from coaches.models import *
 
 class Course(models.Model):
-    name = models.CharField('Название',max_length=200)
-    short_description = models.CharField('Кратное описание',max_length=200)
-    description = models.TextField('Детальная информация')
-    
-    def __str__(self):
-        return self.name
+	name = models.CharField('Название',max_length=200)
+	short_description = models.CharField('Кратное описание',max_length=200)
+	description = models.TextField('Детальная информация')
+	coach = models.ForeignKey(Coach, null = True, blank = True, related_name='coach_courses')
+	assistant = models.ForeignKey(Coach, null = True, blank = True, related_name='assistant_courses')
+	def __str__(self):
+		return self.name
 
 class Lesson(models.Model):
     subject = models.CharField('Тема',max_length=200)
