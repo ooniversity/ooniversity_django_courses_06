f904193eebbc15ddbc5d12229be39c7e29f6e907
Eduard Striukov <edstrkv@gmail.com>
2017-04-01 02:23:41 +0300

    week4/hw7.2.3 - bug fix - deleted 'print'

--- quadratic/views.py
+++ quadratic/views.py
@@ -20,6 +20,7 @@ def quadratic_results(request):
             context['diskr'] = b**2 - 4*a*c
 
             if context['diskr'] < 0:
+                context['flag'] = True
                 context['info'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
             elif context['diskr'] == 0:
                 context['flag'] = True
@@ -30,9 +31,7 @@ def quadratic_results(request):
                 x1 = (-b + context['diskr'] ** (1/2.0)) / 2*a
                 x2 = (-b - context['diskr'] ** (1/2.0)) / 2*a
                 context['info'] = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
-            
-            print(context['diskr'])
-            print(context['info'])
+
     else:
         form = QuadraticForm()
     context['form'] = form
