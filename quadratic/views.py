e3e6b59aea6ba6dac44b5b53eb49ab5456222b9c
Eduard Striukov <edstrkv@gmail.com>
2017-04-01 02:36:10 +0300

    week4/hw7.2.4 - method get in templates

--- quadratic/views.py
+++ quadratic/views.py
@@ -6,8 +6,8 @@ def quadratic_results(request):
 
     context = {}
     info = ''
-    diskr = ''
-    flag = False
+    diskr_value = ''
+
 
     if request.method == "GET":
         form = QuadraticForm(request.GET)
@@ -17,19 +17,19 @@ def quadratic_results(request):
             b = form.cleaned_data['b']
             c = form.cleaned_data['c']
 
-            context['diskr'] = b**2 - 4*a*c
+            context['diskr_value'] = b**2 - 4*a*c
+            context['diskriminant'] = 'Дискриминант: '
+            if context['diskr_value'] < 0:
 
-            if context['diskr'] < 0:
-                context['flag'] = True
                 context['info'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-            elif context['diskr'] == 0:
-                context['flag'] = True
-                x1 = x2 = (b + context['diskr'] ** (1/2.0)) / 2*a
+            elif context['diskr_value'] == 0:
+
+                x1 = x2 = (b + context['diskr_value'] ** (1/2.0)) / 2*a
                 context['info'] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x1
-            elif context['diskr'] > 0:
-                context['flag'] = True
-                x1 = (-b + context['diskr'] ** (1/2.0)) / 2*a
-                x2 = (-b - context['diskr'] ** (1/2.0)) / 2*a
+            elif context['diskr_value'] > 0:
+
+                x1 = (-b + context['diskr_value'] ** (1/2.0)) / 2*a
+                x2 = (-b - context['diskr_value'] ** (1/2.0)) / 2*a
                 context['info'] = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
 
     else:
