59cf1c4539a452b702b8a7908822a88f17b93678
israodin <israodin@gmail.com>
2017-04-01 13:10:44 +0300

    refactoring

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^results/$',views.results, name='results'),
+    url(r'^results/$',views.quadratic_results, name='results'),
 ]
 
