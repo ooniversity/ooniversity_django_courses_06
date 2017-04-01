999857c66045e0afa638ef86abf031365cfadfce
Alexandr Fedorchenko <smolnax@gmail.com>
2017-04-01 18:57:46 +0300

    Kz 7.1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     url(r'^courses/', include('courses.urls')),
     url(r'^students/', include('students.urls')),
     url(r'^contact/$', views.contact, name='contact'),
-    #url(r'^student_list/$', views.student_list, name='student_list'),
+    url(r'^student_list/$', views.student_list, name='student_list'),
     url(r'^student_detail/$', views.student_detail, name='student_detail'),
     url(r'^coaches/', include('coaches.urls')),
 
--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
-from quadratic import views
+
 
 
 
