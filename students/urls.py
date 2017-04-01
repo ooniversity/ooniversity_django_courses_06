ad9a90f18053d1cd6203f8ee5fef730a2eef6386
Zoria1988 <zarina.bondarenko@gmail.com>
2017-04-01 16:55:18 +0300

    Add 7_3

--- students/urls.py
+++ students/urls.py
@@ -6,6 +6,7 @@ app_name = 'students'
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<student_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^add/(?P<pk>\d+)/$', views.add, name='add'),
-
+	url(r'^add/$', views.add, name='add'),
+	url(r'^edit/(?P<id>\d*)/', views.edit, name='edit'),
+	url(r'^remove/(?P<id>\d*)/', views.remove, name='remove'),
 ]
