83974fb8699db3f054c93ff0fa074de2cba919e5
dipperside <figarich@gmail.com>
2017-04-01 20:12:25 +0300

    KZ 7-3: TRY-3

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,26 +1,13 @@
 from django import forms
 
-# Register your models here.
-def isint(value):
-    try:
-        # print("!!!we in tray!!! ",key," : ",kwargs.get(key))
-        int(value)
-        result = True
-    except ValueError:
-        result = False
-    
-    return result
 
 class QuadraticForm(forms.Form):
-    a = forms.CharField(label='коэффициент a', max_length=8)
-    b = forms.CharField(label='коэффициент b', max_length=8)
-    c = forms.CharField(label='коэффициент c', max_length=8)
-    
+    a = forms.IntegerField(label='коэффициент a')
+    b = forms.IntegerField(label='коэффициент b')
+    c = forms.IntegerField(label='коэффициент c')
+
     def clean_a(self):
-        data = self.cleaned_data['a']
-        print('data=',data)
-        if data == '0':
+        a = int(self.cleaned_data['a'])
+        if a == 0:
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
-        elif not isint(data):
-            raise forms.ValidationError("коэффициент не целое число")
-        return data
+        return a
