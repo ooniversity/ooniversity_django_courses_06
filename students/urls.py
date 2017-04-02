cb2079dc070ca18067f6ef95935c7a7fbbeac758
LeusMaximus <leus.maximus@gmail.com>
2017-04-02 14:54:49 +0300

    KZ 7.3 added remove student functionality

--- students/urls.py
+++ students/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^(?P<student_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^add/$', views.create, name='add'),
     url(r'^edit/(?P<student_id>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<student_id>[0-9]+)/$', views.remove, name='remove'),
 ]
