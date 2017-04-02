9ae791bd31b16331de8fd53df4e0067f5c5362a1
dipperside <figarich@gmail.com>
2017-04-02 16:42:41 +0300

    KZ 8: Courses add/change/remove :No1

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from courses import views
 
 app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<id>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<id>\d+)/$', views.remove, name='remove'),
 ]
