7bfb8a2d147d7b72357e44e83970a884cd76619e
KonstRk <qasenior32@gmail.com>
2017-04-02 17:54:57 +0300

    Control task 8 - 1 11

--- courses/urls.py
+++ courses/urls.py
@@ -6,7 +6,7 @@ app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^add/', views.add, name='add'),
-    url(r'^add/(?P<id>\d+)/add_lesson', views.add_lesson, name='add-lesson'),
+    url(r'^(?P<id>\d+)/add_lesson', views.add_lesson, name='add-lesson'),
     url(r'^remove/(?P<id>\d+)/', views.remove, name='remove'),
     url(r'^edit/(?P<id>\d+)/$', views.edit, name='edit'),
 ]
