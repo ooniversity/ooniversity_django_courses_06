dc0ca695b3918ab8218def7b986a0ed7e8e94e0f
LeusMaximus <leus.maximus@gmail.com>
2017-04-02 18:24:52 +0300

    Add widgets to forms

--- courses/forms.py
+++ courses/forms.py
@@ -7,9 +7,24 @@ class CourseModelForm(forms.ModelForm):
 
         fields = ['name', 'short_description', 'description', 'coach', 'assistant']
 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'short_description': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'coach': forms.Select(attrs={'class': 'form-control'}),
+            'assistant': forms.Select(attrs={'class': 'form-control'}),
+        }
+
 
 class LessonModelForm(forms.ModelForm):
     class Meta:
         model = Lesson
 
         fields = ['subject', 'description', 'course', 'order']
+
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'course': forms.Select(attrs={'class': 'form-control'}),
+            'order': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
