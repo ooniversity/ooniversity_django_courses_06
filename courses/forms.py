4033d8fb0ed36d5a196485d94f8b4fa741dcb736
VOlni <salivan13@gmail.com>
2017-04-02 23:27:49 +0300

    kz 8

---
+++ courses/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from . models import Course, Lesson
+
+class CourseModelForm(forms.ModelForm):
+    class Meta:
+        model = Course
+
+        fields = ['name', 'short_description', 'description', 'coach', 'assistant']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'short_description': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'coach': forms.Select(attrs={'class': 'form-control'}),
+            'assistant': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+class LessonModelForm(forms.ModelForm):
+    class Meta:
+        model = Lesson
+
+        fields = ['subject', 'description', 'course', 'order']
+
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'course': forms.Select(attrs={'class': 'form-control'}),
+            'order': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
---
+++ students/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from . models import Student
+
+
+class StudentModelForm(forms.ModelForm):
+    class Meta:
+        model = Student
+
+        fields = ['name', 'surname', 'date_of_birth', 'email', 'phone', 'address', 'skype', 'courses']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'surname': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_of_birth': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'skype': forms.TextInput(attrs={'class': 'form-control'}),
+            'courses': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+
+        help_texts = {
+            'courses': 'Hold down "Control", or "Command" on a Mac, to select more than one.',
+        }
