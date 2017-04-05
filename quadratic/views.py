940872ade172ab01e95f9998e290de840b537c37
fedorchenko-a <smolnax@gmail.com>
2017-04-05 19:00:46 +0300

    Update views.py

--- quadratic/views.py
+++ quadratic/views.py
@@ -10,8 +10,8 @@ from . forms import QuadraticForm
 def quadratic_results(request):
     message = {}
     diskr = ''
-    if request.method == "POST":
-        form = QuadraticForm(request.POST)
+    if request.method == "GET":
+        form = QuadraticForm(request.GET)
         
         
         if form.is_valid():
