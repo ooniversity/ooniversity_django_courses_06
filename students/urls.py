79bf112bf264305282b354a0cfa396f393c4b2a3
dipperside <figarich@gmail.com>
2017-04-08 01:04:10 +0300

    KZ 9.1: CRUD students modules-1

--- students/urls.py
+++ students/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 from students import views
 
 app_name = 'students'
-urlpatterns = [
-    url(r'^$', views.list_view, name='list_view'),
-    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),# 
-    url(r'^add/$', views.create, name='add'),
-    url(r'^edit/(?P<id>\d+)/$', views.edit, name='edit'),
-    url(r'^remove/(?P<id>\d+)/$', views.remove, name='remove'),
 
+urlpatterns = [
+    # url(r'^$', views.list_view, name='list_view'),
+    # url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    # url(r'^add/$', views.create, name='add'),
+    url(r'^$', views.StudentListView.as_view(), name='list_view'),
+    url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='detail'),
+    url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='remove'),
 ]
