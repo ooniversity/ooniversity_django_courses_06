178f387bba755c251e6561ffbeb482134ced11eb
KonstRk <qasenior32@gmail.com>
2017-04-08 12:09:35 +0300

    Control task 10 - 1 2

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -4,24 +4,24 @@ from feedbacks.models import Feedback
 from feedbacks.forms import FeedbackModelForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.core.mail import mail_admins
 
-class FeedbackView(SuccessMessageMixin, CreateView):
+class FeedbackView(CreateView):
     model = Feedback
     template_name = 'feedback/feedback.html'
     form_class = FeedbackModelForm
-    success_message = "Thank you for your feedback! We will keep in touch with you very soon!"
-    success_url = 'feedback'
+#    success_message = "Thank you for your feedback! We will keep in touch with you very soon!"
+    success_url = reverse_lazy('feedback')
 
     def form_valid(self, form):
         self.object = form.save()
-        data = form
         response = super().form_valid(form)
         sendMail = mail_admins(
-        	self.object.subject,
-        	self.object.message,
-        	fail_silently=False)
+            self.object.subject,
+            self.object.message,
+            self.object.from_email)
+        messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!')
         return response
 
     def get_context_data(self, **kwargs):
