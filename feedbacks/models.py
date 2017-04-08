2dbe126a367a70f83fa10238d7045348ae84474d
KonstRk <qasenior32@gmail.com>
2017-04-08 12:52:26 +0300

    Control task 10 - 1 6

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -5,7 +5,7 @@ class Feedback(models.Model):
     subject  = models.CharField(max_length=255)
     message = models.TextField()
     from_email = models.EmailField()
-    create_date = models.DateField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
