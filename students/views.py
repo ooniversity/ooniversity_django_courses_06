98f874936acb7658c78b75247d4f4b715a1d0f9c
dipperside <figarich@gmail.com>
2017-04-08 16:03:44 +0300

    KZ 10.2: sweet pager No1

--- students/views.py
+++ students/views.py
@@ -20,6 +20,7 @@ class StudentDetailView(DetailView):
 
 class StudentListView(ListView):
     model = Student
+    paginate_by = 2
 
     def get_queryset(self):
         qs = super().get_queryset()
