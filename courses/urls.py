8b463cbbafcf1aeaf9d20a192563bdf672381ebb
israodin <israodin@gmail.com>
2017-04-10 00:18:10 +0300

    add apps  student course && lesson   to  admin.py

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from . import views
 urlpatterns =[
     url(r'^',views.list_courses,name ='list_of_courses'),
-    url(r'^courses/<a>',views.lessons_list,name = 'lessons_list'),
+    url(r'^(?P<pk>\d+)/$',views.lessons_list,name = 'lessons_list'),
 ]
