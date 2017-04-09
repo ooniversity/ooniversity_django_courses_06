6fe1c511313af92eace8b1f0e9b2260bf3b37fb1
mazart <am.magicsmile@gmail.com>
2017-04-09 17:19:07 +0300

    Task 10.2 v.2

--- coaches/views.py
+++ coaches/views.py
@@ -4,6 +4,6 @@ from courses.models import Course
 
 def detail(request, coach_id):
 	coach = Coach.objects.get(id=coach_id)
-	teacher = Course.objects.filter(coach=coach_id)
-	assistant = Course.objects.filter(assistant=coach_id)
-	return render(request, 'coaches/detail.html', {'coach':coach, 'teacher':teacher, 'assistant':assistant})
+	teacher = Course.objects.filter(coach_id=coach_id)
+	assistant = Course.objects.filter(assistant_id=coach_id)
+	return render(request, 'coaches/detail.html', {'coach': coach, 'teacher': teacher, 'assistant': assistant})
