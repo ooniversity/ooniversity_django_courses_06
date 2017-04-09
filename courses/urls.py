d25116914622d48f4c944b8dd68c70c0655d98bc
Burlakov Alexey <alex.tsumibito@gmail.com>
2017-04-09 12:47:22 +0300

    kz-9.2 v.3

--- courses/urls.py
+++ courses/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
     url(r'^edit/(?P<pk>\d+)/$', views.CourseUpdateView.as_view(), name='edit'),
     url(r'^remove/(?P<pk>\d+)/$', views.CourseDeleteView.as_view(), name='remove'),
-    url(r'^(?P<pk>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
+    url(r'^(?P<course_id>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
     url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name='detail'),
 ]
