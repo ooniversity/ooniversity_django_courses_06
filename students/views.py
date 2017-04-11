4197e87c958769f3766cfa8c2a7bb1b2e3b97fc1
Maksym Yatsenko <maxy.1982@gmail.com>
2017-04-11 17:08:54 +0300

    Task11_1

--- students/views.py
+++ students/views.py
@@ -7,7 +7,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+import logging
 
+logger = logging.getLogger('students')
 
 class StudentListView(ListView):
     model = Student
@@ -33,6 +35,11 @@ class StudentListView(ListView):
 
 class StudentDetailView(DetailView):
     model = Student
+    def get_context_data(self, **kwargs):
+        logger.debug('Students detail view has been debugged!')
+        logger.info('Logger of students detail view informs you!')
+        logger.warning('Logger of students detail view warns you!')
+        logger.error('Students detail view went wrong!')
 
 class StudentCreateView(CreateView):
     model = Student
