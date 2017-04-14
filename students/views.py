7f4bd890945c68e6aa0d47ec665c3b014306bfe7
LeusMaximus <leus.maximus@gmail.com>
2017-04-15 01:18:44 +0300

    KZ 9.1 Added context to StudentCreateView

--- students/views.py
+++ students/views.py
@@ -118,6 +118,13 @@ class StudentCreateView(CreateView):
 
         return response
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['title'] = 'Student registration'
+
+        return context
+
 
 def create(request):
     '''
