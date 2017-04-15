f6aa79537c266b4b8a5a97db17e3c30331a23c7a
dipperside <figarich@gmail.com>
2017-04-15 23:49:37 +0300

    KZ 11.2: Testing 1-2-3 :No2

--- courses/tests.py
+++ courses/tests.py
@@ -7,51 +7,75 @@ from courses.models import Course, Lesson
 class CoursesListTest(TestCase):
 
     def test_list_one(self):
-        response = self.client.get('/courses/1/')
-        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
     
     def test_list_two(self):
-        add_course1 = Course.objects.create(name = 'kurs-1',
-                                           short_description = 'ababagalamaga ababagalamaga ababagalamaga',
-                                           )
-        response = self.client.get('/courses/1/')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response,'kurs-1')
+        response = self.client.get('/')
+        self.assertContains(response, 'Вітаємо сайті')
   
     def test_list_three(self):
-        pass
+        response = self.client.get('/')
+        self.assertContains(response, 'ПЕРЕЛІК КУРСІВ')
 
     def test_list_four(self):
-        pass
+        response = self.client.get('/')
+        self.assertContains(response, 'href="/courses/add/"')
 
     def test_list_five(self):
-        pass
+        add_course1 = Course.objects.create(name = 'kurs-1',
+                                            short_description = 'ababagalamaga',                                           )
+        response = self.client.get('/')
+        self.assertContains(response, 'href="/courses/edit/1/"')
 
     def test_list_six(self):
-        pass
+        add_course1 = Course.objects.create(name = 'kurs-1',
+                                            short_description = 'ababagalamaga',                                           )
+        response = self.client.get('/')
+        self.assertContains(response, 'href="/courses/remove/1/"')
     
     
 class CoursesDetailTest(TestCase):
     
     def test_detail_one(self):
-        pass
+        response = self.client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+
 
     def test_detail_two(self):
-        pass
+        add_course1 = Course.objects.create(name = 'kurs-1',
+                                            short_description = 'ababagalamaga',
+                                            )
+        response = self.client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,'kurs-1')
 
 
     def test_detail_three(self):
-        pass
-
+        add_course1 = Course.objects.create(name = 'kurs-1',
+                                            short_description = 'ababagalamaga',
+                                            )
+        response = self.client.get('/courses/1/')
+        self.assertContains(response,'href="/courses/1/add_lesson"')        
 
     def test_detail_four(self):
-        pass
+        add_course1 = Course.objects.create(name = 'kurs-1',
+                                            short_description = 'ababagalamaga',
+                                            )
+        response = self.client.get('/courses/1/add_lesson')
+        self.assertContains(response,'kurs-1')        
 
 
     def test_detail_five(self):
-        pass
-
-
-    def test_detail_six(self):
-        pass
+        add_course1 = Course.objects.create(name = 'kurs-1',
+                                            short_description = 'ababagalamaga',
+                                            )
+        add_lesson1 = Lesson.objects.create(subject = 'UROK KUROK',
+                                            description = 'opis uroku',
+                                            course = add_course1,
+                                            order = '1',)
+        response = self.client.get('/courses/1/')
+        self.assertContains(response,'opis uroku')
         
+
